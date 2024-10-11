c6bd59451836432fe395be58fa86455d611c0814
ihor1 <ihor1006@gmail.com>
2024-10-11 17:09:24 +0200

    scale first header&banner update

---
+++ Prod/FirstSection/sectionOne.js
@@ -0,0 +1,47 @@
+const sectionOne = document.createElement('section');
+sectionOne.className = 'sectionOne';
+
+
+const title = document.createElement('div');
+title.className = 'title';
+title.innerHTML = `
+    <h1 class="comfort-test-description">Free In-Store Comfort Test</h1>
+    <p class="comfort-test-info">Visit one of our stores for a free, personalized, expert-guided comfort test.</p>
+`;
+
+
+const infoDiv = document.createElement('div');
+infoDiv.className = 'infoDiv';
+
+
+const photoSection = document.createElement('img');
+photoSection.className = 'photo';
+photoSection.src = '../svg/photoSection.png';
+photoSection.alt = 'photo';
+infoDiv.appendChild(photoSection);
+
+
+const infoPhoto = document.createElement('div');
+infoPhoto.className = 'infoPhoto';
+infoPhoto.innerHTML = `
+    <h2 class="try-before-you-buy">Try before you buy</h2>
+    <p class="info">
+        When it comes to purchasing a new mattress, it's tempting to browse<br/>
+         online and make a selection based on reviews and descriptions. While<br/>
+          online shopping offers convenience and a wide range of options, there's a<br/>
+           compelling reason why it's crucial to try a mattress in-store before making <br/>
+           that important decision. 
+           <br/><br/>
+           At Sleep Shop, we have a designated Comfort Test area in our stores, <br/>
+           where we educate you on how to find the right mattress based on some <br/>
+           key principles. It is an educational, relaxing, and simple process.<br/>
+    </p>
+    <button class="findStoreBtn">FIND A STORE NEAR YOU</button>
+`;
+infoDiv.appendChild(infoPhoto);
+
+
+sectionOne.appendChild(title);
+sectionOne.appendChild(infoDiv);
+document.body.appendChild(sectionOne);
+
---
+++ Prod/Header/Header.js
@@ -0,0 +1,100 @@
+const banner = document.createElement('div');
+banner.innerText = 'BUY NOW, PAY LATER | Back To School Sale Is On';
+banner.className = 'banner';
+
+const closeBanner = document.createElement("a");
+closeBanner.innerText = 'х';
+closeBanner.className = 'close-banner';
+banner.appendChild(closeBanner);
+
+
+closeBanner.onclick = function (event) {
+    event.preventDefault();
+    banner.style.height = '0';
+    banner.style.overflow = 'hidden';
+    closeBanner.style.display = 'none';
+}
+
+const header = document.createElement('header');
+header.className = 'header';
+header.appendChild(banner)
+
+
+const firstHeader = document.createElement('div');
+firstHeader.className = 'firstHeader';
+header.appendChild(firstHeader);
+
+
+const logo = document.createElement('img');
+logo.className = 'Logo';
+logo.src = '../svg/ShopLogo.svg';
+logo.alt = 'logo';
+firstHeader.appendChild(logo);
+
+
+const info = document.createElement('div');
+info.className = 'Info';
+info.innerHTML = `
+    <a href="https://www.google.com/maps/place/Richmond" target="_blank" class="Richmond">
+        <img class="Vector" src="../../svg/Vector.svg" alt="Vector">Richmond
+        <img class="secondVector" src="../../svg/VectorSecond.svg" alt="Second Vector">
+    </a>
+    <h8 class="time">10:00 AM - 7:00 PM</h8>
+`;
+firstHeader.appendChild(info);
+
+
+const searchDiv = document.createElement('div');
+searchDiv.className = 'searchDiv';
+searchDiv.innerHTML = `
+    <input type="text" class="searchInput" placeholder="Search Products">
+    <button class="searchButton">
+        <img src="../../svg/SearchButton.svg" alt="Search" class="searchIcon">
+    </button>
+`;
+firstHeader.appendChild(searchDiv);
+
+
+const cart = document.createElement('img');
+cart.className = 'Cart';
+cart.src = '../svg/Cart.svg';
+cart.alt = 'cart';
+
+
+const cartDiv = document.createElement('div');
+cartDiv.className = 'cartDiv';
+cartDiv.appendChild(cart);
+firstHeader.appendChild(cartDiv);
+
+
+const secondHeader = document.createElement('div');
+secondHeader.className = 'secondHeader';
+
+
+const sleepFurniture = document.createElement('div');
+sleepFurniture.className = 'sleepFurniture';
+sleepFurniture.innerHTML = `
+    <div class="furnitureBlock"><a href="#">MATTRESSES</a></div>
+    <div><a href="#">ADJUSTABLE BASES</a></div>
+    <div><a href="#">PILLOWS</a></div>
+    <div><a href="#">BEDDING</a></div>
+    <div><a href="#">SLEEP FURNITURE</a></div>
+`;
+
+
+const schoolBlock = document.createElement('div');
+schoolBlock.className = 'schoolBlock';
+schoolBlock.innerHTML = `
+    <div class="backToSchool"><a href="#">BACK TO SCHOOL SALE</a></div>
+    <div class="clearance"><a href="#">CLEARANCE</a></div>
+`;
+
+
+secondHeader.append(sleepFurniture, schoolBlock);
+header.appendChild(secondHeader);
+
+
+document.body.appendChild(header);
+
+
+
